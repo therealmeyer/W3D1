c9e104c5618734360b02a590eb78ec879a8cf765
therealmeyer <rmeyer@scu.edu>
2018-05-01 09:54:29 -0700

    Complete some 09_self_joins.rb exercises

--- sql_zoo/lib/09_self_joins.rb
+++ sql_zoo/lib/09_self_joins.rb
@@ -17,18 +17,38 @@ require_relative './sqlzoo.rb'
 def num_stops
   # How many stops are in the database?
   execute(<<-SQL)
+  SELECT
+    COUNT(id)
+  FROM
+    stops
   SQL
 end
 
 def craiglockhart_id
   # Find the id value for the stop 'Craiglockhart'.
   execute(<<-SQL)
+  SELECT
+    DISTINCT stop_id
+  FROM
+    routes
+  JOIN
+    stops ON routes.stop_id = stops.id
+  WHERE
+    stops.name = 'Craiglockhart'
   SQL
 end
 
 def lrt_stops
   # Give the id and the name for the stops on the '4' 'LRT' service.
   execute(<<-SQL)
+  SELECT
+    stops.id, stops.name
+  FROM
+    stops
+  JOIN
+    routes ON routes.stop_id = stops.id
+  WHERE
+    routes.num = '4'
   SQL
 end
 
@@ -51,6 +71,16 @@ def connecting_routes
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
   execute(<<-SQL)
+  SELECT
+    company, num, COUNT(*)
+  FROM
+    routes
+  WHERE
+    stop_id = 149 OR stop_id = 53
+  GROUP BY
+    company, num
+  HAVING
+    COUNT(*) = 2
   SQL
 end
 
